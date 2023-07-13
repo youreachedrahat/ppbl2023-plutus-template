084b8bcb695fe6c7c57a7fc0b029335498f58afc
mario.jeyren@fixedfor.com <mario.jeyren@fixedfor.com>
2023-07-13 08:10:56 +0000

    waddr and keys

--- bash-scripts/variables-example.sh
+++ bash-scripts/variables-example.sh
@@ -3,8 +3,8 @@
 # Make a copy of this file with the name variables-private.sh
 # Then, set the CARDANO_NODE_SOCKET_PATH to match your local set-up
 
-export CARDANO_NODE_SOCKET_PATH=<YOUR PATH TO>/db/node.socket
+# export CARDANO_NODE_SOCKET_PATH=<YOUR PATH TO>/db/node.socket
 
 # Optionally, you can add your CLI address and private key to a file like this, for more efficient accces:
-# export SENDERADDRESS=""
-# export SENDERKEY=""
+export SENDERADDRESS="addr_test1qrw2cf9kcxnggz7yrvfuxsscvwuh3mfx4mcndjr4s9n93fsuzzw3meyas87hd5l8y8pl209l99dfv0m4y5kvpdt0ym5s7v9pnm"
+export SENDERKEY="~/workspace/repo/wallet/payment.skey"
